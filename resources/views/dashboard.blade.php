890371c90b112905bab3406a0c32bb4a4ee3fb5e
aji sampurno <ajisampurno121@gmail.com>
2023-10-13 10:44:22 +0700

    membersihkan dashboard controller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,22 +11,7 @@
           <div class="card bg-gradient-info text-black">
             <div class="card-body" align="center">
               <h3 class="mb-2">Hari ini: <p id="waktu"></p></h3>
-              <div class="row">
-
-                @foreach ($abseninout as $inout)                    
-                  <div class="col-lg-6 mt-3">
-                    Absen masuk <br>
-                    <img style="width: 70px" class="rounded-circle" src="{{ $inout->gambar_in }}" alt=""><br>
-                     {{ $inout->time_in }}
-                  </div>
-                  <div class="col-lg-6 mt-3">
-                    Absen keluar <br>
-                    <img style="width: 70px" class="rounded-circle" src="{{ $inout->gambar_out }}" alt=""><br>
-                    {{ $inout->time_out }}
-                  </div>
-                </div>
-                @endforeach
-              
+              <div class="row">              
               <h3 class="mt-4">Lokasi Anda:</h3>
               <p id="lokasi"></p>
 
@@ -39,16 +24,6 @@
               </a>
             </div>
           </div>
-
-          
-              <!--<h5 class="font-weight-normal mb-1">~ Quote of the day ~</h5>
-              <h5 class="font-weight-normal mb-1" id="quotes"></h5>
-              <h5 class="font-weight-normal mb-1" id="byquotes"></h5>
-              <h5 class="font-weight-normal mb-3" id="animequotes"></h5>
-              <b class="font-weight-bold" href="{{url('/')}}"> Visitor</b>
-              <a href='http://www.freevisitorcounters.com'>www.freevisitorcounters.com</a> 
-              <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=24e5a2da427c0ecde6691d52e75a72fdd856849b'></script>
-              <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1000708/t/1"></script> -->
         </div>
       </div>
     </div>
